8df93b57abc062cc3521b63e5bac5ee244575f72
Darrell <darrell@cratis.nl>
2021-09-25 17:45:37 +0200

    changed the shutdown command

--- roles/rpi_lcd/files/buttons.py
+++ roles/rpi_lcd/files/buttons.py
@@ -6,7 +6,7 @@ button2 = Button(23)
 button3 = Button(24)
 
 reboot_statement = 'logger "System reboot initiatied by button press"; sudo reboot'
-shutdown_statement = 'logger "System shutdown initiated by button press"; sudo shutdown -h'
+shutdown_statement = 'logger "System shutdown initiated by button press"; sudo shutdown now'
 
 while True:
     if button2.is_pressed:
